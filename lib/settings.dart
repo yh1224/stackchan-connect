1009b9f154772f97f7a647c0a6c3255d28d43097
Yoshiharu Hirose <yh1224@gmail.com>
2023-03-25 11:46:50 +0900

    implement

---
+++ lib/settings.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SettingsPage extends StatefulWidget {
+  const SettingsPage({super.key});
+
+  @override
+  State<SettingsPage> createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  final stackchanIpAddressTextArea = TextEditingController();
+
+  String errorMessage = '';
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    restoreSettings();
+    super.initState();
+  }
+
+  void saveSettings() async {
+    var prefs = await SharedPreferences.getInstance();
+    await prefs.setString('stackchanIpAddress', stackchanIpAddressTextArea.text);
+  }
+
+  void restoreSettings() async {
+    var prefs = await SharedPreferences.getInstance();
+    setState(() {
+      stackchanIpAddressTextArea.text = prefs.getString('stackchanIpAddress') ?? '';
+    });
+  }
+
+  void test() async {
+    var stackchanIpAddress = stackchanIpAddressTextArea.text;
+    if (stackchanIpAddress.isNotEmpty) {
+      try {
+        setState(() {
+          errorMessage = 'Connecting...';
+          isLoading = true;
+        });
+        var res = await http.post(Uri.http(stackchanIpAddress, "/speech"), body: {
+          "say": "接続できました",
+        });
+        if (res.statusCode != 200) {
+          setState(() {
+            errorMessage = 'Error: ${res.statusCode}';
+          });
+        }
+        setState(() {
+          errorMessage = 'OK';
+        });
+      } catch (e) {
+        setState(() {
+          errorMessage = 'Error: ${e.toString()}';
+        });
+      } finally {
+        setState(() {
+          isLoading = false;
+        });
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ｽﾀｯｸﾁｬﾝ ｺﾝﾈｸﾄ'),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              const Text("ｽﾀｯｸﾁｬﾝの IP アドレスを入力してください"),
+              TextField(
+                autofocus: true,
+                controller: stackchanIpAddressTextArea,
+                style: const TextStyle(fontSize: 20),
+                onChanged: (String value) {
+                  saveSettings();
+                },
+              ),
+              ValueListenableBuilder(
+                valueListenable: stackchanIpAddressTextArea,
+                builder: (context, value, child) {
+                  return ElevatedButton(
+                    onPressed: stackchanIpAddressTextArea.text.isEmpty || isLoading ? null : test,
+                    child: const Text(
+                      'Test',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  );
+                },
+              ),
+              Text(errorMessage),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/speech.dart
@@ -0,0 +1,205 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:speech_to_text/speech_recognition_error.dart';
+import 'package:speech_to_text/speech_recognition_result.dart';
+import 'package:speech_to_text/speech_to_text.dart' as stt;
+
+class SpeechPage extends StatefulWidget {
+  const SpeechPage(this.message, this.apiPath, this.parameterKey, {super.key});
+
+  final String message;
+  final String apiPath;
+  final String parameterKey;
+
+  @override
+  State<SpeechPage> createState() => _SpeechPageState();
+}
+
+class _SpeechPageState extends State<SpeechPage> {
+  final textArea = TextEditingController();
+  final List<String> result = [];
+  String sttStatus = '';
+  bool isListening = false;
+  bool isLoading = false;
+  final stt.SpeechToText speech = stt.SpeechToText();
+
+  @override
+  void dispose() {
+    textArea.dispose();
+    super.dispose();
+  }
+
+  // 音声入力開始
+  Future<void> startListening() async {
+    bool available = await speech.initialize(
+      onError: errorListener,
+      onStatus: statusListener,
+    );
+    if (available) {
+      speech.listen(onResult: resultListener);
+      setState(() {
+        sttStatus = '音声入力中...';
+        isListening = true;
+      });
+    } else {
+      sttStatus = "音声入力が拒否されました。";
+    }
+  }
+
+  // 音声入力停止
+  Future<void> stopListening() async {
+    speech.stop();
+    setState(() {
+      isListening = false;
+    });
+  }
+
+  // 音声入力結果
+  void resultListener(SpeechRecognitionResult result) {
+    setState(() {
+      textArea.text = result.recognizedWords;
+      sttStatus = '';
+    });
+  }
+
+  // 音声入力エラー
+  void errorListener(SpeechRecognitionError error) {
+    setState(() {
+      sttStatus = '${error.errorMsg} - ${error.permanent}';
+      isListening = false;
+    });
+  }
+
+  // 音声入力状態
+  void statusListener(String status) {
+    if (status == 'done') {
+      setState(() {
+        sttStatus = '';
+        isListening = false;
+      });
+    } else {
+      setState(() {
+        sttStatus = status;
+      });
+    }
+  }
+
+  // ｽﾀｯｸﾁｬﾝ API を呼ぶ
+  void callStackchan() async {
+    var prefs = await SharedPreferences.getInstance();
+    try {
+      var stackchanIpAddress = prefs.getString('stackchanIpAddress');
+      if (stackchanIpAddress != null && stackchanIpAddress.isNotEmpty) {
+        setState(() {
+          isLoading = true;
+        });
+        var message = textArea.text;
+        textArea.text = '';
+        result.add('> $message');
+        var res = await http.post(Uri.http(stackchanIpAddress, widget.apiPath), body: {
+          widget.parameterKey: message,
+        });
+        if (res.statusCode != 200) {
+          setState(() {
+            result.add('Error: ${res.statusCode}');
+          });
+        }
+        var body = utf8.decode(res.bodyBytes);
+        var si = body.indexOf("<body>");
+        var ei = body.indexOf("</body>");
+        if (si >= 0 && ei >= 0) {
+          result.add(body.substring(si + 6, ei));
+        }
+      } else {
+        result.add('ｽﾀｯｸﾁｬﾝ の IP アドレスが設定されていません');
+      }
+    } catch (e) {
+      setState(() {
+        result.add('Error: ${e.toString()}');
+      });
+    } finally {
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
+
+  // 入力をクリア
+  void clear() {
+    setState(() {
+      textArea.clear();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ｽﾀｯｸﾁｬﾝ ｺﾝﾈｸﾄ'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Text(widget.message),
+                  Expanded(
+                    child: ListView(
+                      padding: const EdgeInsets.all(8.0),
+                      children: result.map((r) => Text(r, style: const TextStyle(fontSize: 16))).toList(),
+                    ),
+                  ),
+                  Text(sttStatus),
+                ],
+              ),
+            ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Flexible(
+                      child: TextField(
+                        autofocus: true,
+                        controller: textArea,
+                        keyboardType: TextInputType.multiline,
+                        maxLines: null,
+                        style: const TextStyle(fontSize: 20),
+                      ),
+                    ),
+                    ValueListenableBuilder(
+                      valueListenable: textArea,
+                      builder: (context, value, child) {
+                        return IconButton(
+                          color: Colors.blue,
+                          icon: textArea.text.isEmpty
+                              ? (isListening ? const Icon(Icons.stop) : const Icon(Icons.mic))
+                              : const Icon(Icons.send),
+                          onPressed: isLoading
+                              ? null
+                              : (textArea.text.isEmpty
+                                  ? isListening
+                                      ? stopListening
+                                      : startListening
+                                  : callStackchan),
+                        );
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
