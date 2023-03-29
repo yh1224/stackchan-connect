6b7773ca3126e6fe9662bc8910db95f9fb6b5372
Yoshiharu Hirose <yh1224@gmail.com>
2023-03-30 08:35:27 +0900

    add drawer and links

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:url_launcher/link.dart';
 
 import 'settings.dart';
 import 'speech.dart';
@@ -34,6 +35,50 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('ｽﾀｯｸﾁｬﾝ ｺﾝﾈｸﾄ'),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(color: Colors.blueAccent),
+              child: Text(
+                "ｽﾀｯｸﾁｬﾝ ｺﾝﾈｸﾄ",
+                style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            // ListTile(
+            //   title: const Text("設定"),
+            //   onTap: () {
+            //     Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+            //       return const SettingsPage();
+            //     }));
+            //   },
+            // ),
+            Link(
+              uri: Uri.parse('https://notes.yh1224.com/stackchan-connect/'),
+              target: LinkTarget.blank,
+              builder: (BuildContext ctx, FollowLink? openLink) {
+                return ListTile(
+                  title: const Text("アプリについて／使い方"),
+                  onTap: openLink,
+                );
+              },
+            ),
+            Link(
+              uri: Uri.parse('https://notes.yh1224.com/privacy/'),
+              target: LinkTarget.blank,
+              builder: (BuildContext ctx, FollowLink? openLink) {
+                return ListTile(
+                  title: const Text("プライバシーポリシー"),
+                  onTap: openLink,
+                );
+              },
+            ),
+          ],
+        ),
       ),
       body: SingleChildScrollView(
         child: Padding(
