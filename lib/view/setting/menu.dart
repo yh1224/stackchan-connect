aa1ac9cc56fe363f5259cd90fba40cc84b770ae3
Yoshiharu Hirose <yh1224@gmail.com>
2023-04-17 07:36:36 +0900

    menu scrollable

--- lib/view/setting/menu.dart
+++ lib/view/setting/menu.dart
@@ -44,73 +44,75 @@ class _SettingMenuPageState extends State<SettingMenuPage> {
       body: GestureDetector(
         child: Visibility(
           visible: _initialized,
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  children: [
-                    Card(
-                      child: ListTile(
-                        title: Text("IP アドレス設定", style: Theme.of(context).textTheme.titleLarge),
-                        subtitle: Text(_stackchanIpAddress),
-                        onTap: () async {
-                          await Navigator.of(context)
-                              .push(MaterialPageRoute(builder: (context) => const SettingIpAddressPage()));
-                          _init();
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Visibility(
-                visible: _stackchanIpAddress.isNotEmpty,
-                child: Padding(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: Column(
                     children: [
                       Card(
                         child: ListTile(
-                          title: Text("API Key 設定", style: Theme.of(context).textTheme.titleLarge),
-                          onTap: () {
-                            Navigator.of(context)
-                                .push(MaterialPageRoute(builder: (context) => SettingApiKeyPage(_stackchanIpAddress)));
-                          },
-                        ),
-                      ),
-                      Card(
-                        child: ListTile(
-                          title: Text("ロール設定", style: Theme.of(context).textTheme.titleLarge),
-                          onTap: () {
-                            Navigator.of(context)
-                                .push(MaterialPageRoute(builder: (context) => SettingRolePage(_stackchanIpAddress)));
-                          },
-                        ),
-                      ),
-                      Card(
-                        child: ListTile(
-                          title: Text("音量設定", style: Theme.of(context).textTheme.titleLarge),
-                          onTap: () {
-                            Navigator.of(context).push(
-                                MaterialPageRoute(builder: (context) => SettingStackchanPage(_stackchanIpAddress)));
-                          },
-                        ),
-                      ),
-                      Card(
-                        child: ListTile(
-                          title: Text("声色設定", style: Theme.of(context).textTheme.titleLarge),
-                          onTap: () {
-                            Navigator.of(context)
-                                .push(MaterialPageRoute(builder: (context) => SettingVoicePage(_stackchanIpAddress)));
+                          title: Text("IP アドレス設定", style: Theme.of(context).textTheme.titleLarge),
+                          subtitle: Text(_stackchanIpAddress),
+                          onTap: () async {
+                            await Navigator.of(context)
+                                .push(MaterialPageRoute(builder: (context) => const SettingIpAddressPage()));
+                            _init();
                           },
                         ),
                       ),
                     ],
                   ),
                 ),
-              ),
-            ],
+                Visibility(
+                  visible: _stackchanIpAddress.isNotEmpty,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Card(
+                          child: ListTile(
+                            title: Text("API Key 設定", style: Theme.of(context).textTheme.titleLarge),
+                            onTap: () {
+                              Navigator.of(context)
+                                  .push(MaterialPageRoute(builder: (context) => SettingApiKeyPage(_stackchanIpAddress)));
+                            },
+                          ),
+                        ),
+                        Card(
+                          child: ListTile(
+                            title: Text("ロール設定", style: Theme.of(context).textTheme.titleLarge),
+                            onTap: () {
+                              Navigator.of(context)
+                                  .push(MaterialPageRoute(builder: (context) => SettingRolePage(_stackchanIpAddress)));
+                            },
+                          ),
+                        ),
+                        Card(
+                          child: ListTile(
+                            title: Text("音量設定", style: Theme.of(context).textTheme.titleLarge),
+                            onTap: () {
+                              Navigator.of(context).push(
+                                  MaterialPageRoute(builder: (context) => SettingStackchanPage(_stackchanIpAddress)));
+                            },
+                          ),
+                        ),
+                        Card(
+                          child: ListTile(
+                            title: Text("声色設定", style: Theme.of(context).textTheme.titleLarge),
+                            onTap: () {
+                              Navigator.of(context)
+                                  .push(MaterialPageRoute(builder: (context) => SettingVoicePage(_stackchanIpAddress)));
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
